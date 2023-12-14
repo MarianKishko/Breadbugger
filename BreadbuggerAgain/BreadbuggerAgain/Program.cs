a802713c3f9daf95c7851bff126e7ccd1c78f9c5
Marian <mkisko004@gmail.com>
2023-12-14 18:37:16 +0100

    Breadbug is inited

---
+++ BreadbuggerAgain/BreadbuggerAgain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BreadbuggerCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Breadbug!");
+        }
+    }
+}
